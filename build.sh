57a3cfb9ba5c2a4140c0d9d1e52ac3c8ccc7d910
bluish55 <bmkim031004@gmail.com>
2026-04-18 03:12:01 +0900

    fix anonKey whitespace

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 node -e "
 const fs = require('fs');
-const config = 'const CONFIG = {\n  kakao: {\n    jsKey: \"' + process.env.KAKAO_JS_KEY + '\",\n    restKey: \"' + process.env.KAKAO_REST_KEY + '\"\n  },\n  supabase: {\n    url: \"' + process.env.SUPABASE_URL + '\",\n    anonKey: \"' + process.env.SUPABASE_ANON_KEY + '\"\n  }\n};';
+const anonKey = (process.env.SUPABASE_ANON_KEY || '').replace(/\s/g, '');
+const config = 'const CONFIG = {\n  kakao: {\n    jsKey: \"' + process.env.KAKAO_JS_KEY + '\",\n    restKey: \"' + process.env.KAKAO_REST_KEY + '\"\n  },\n  supabase: {\n    url: \"' + process.env.SUPABASE_URL + '\",\n    anonKey: \"' + anonKey + '\"\n  }\n};';
 fs.writeFileSync('config.js', config);
 "
