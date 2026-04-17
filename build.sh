90844737b67a2c5997c1c5ff6bf1fadb9ae5808f
bluish55 <bmkim031004@gmail.com>
2026-04-18 02:58:28 +0900

    fix build.sh line break issue

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-cat > config.js << EOF
-const CONFIG = {
-  kakao: {
-    jsKey: '${KAKAO_JS_KEY}',
-    restKey: '${KAKAO_REST_KEY}'
-  },
-  supabase: {
-    url: '${SUPABASE_URL}',
-    anonKey: '${SUPABASE_ANON_KEY}'
-  }
-};
-EOF
+node -e "
+const fs = require('fs');
+const config = 'const CONFIG = {\n  kakao: {\n    jsKey: \"' + process.env.KAKAO_JS_KEY + '\",\n    restKey: \"' + process.env.KAKAO_REST_KEY + '\"\n  },\n  supabase: {\n    url: \"' + process.env.SUPABASE_URL + '\",\n    anonKey: \"' + process.env.SUPABASE_ANON_KEY + '\"\n  }\n};';
+fs.writeFileSync('config.js', config);
+"
